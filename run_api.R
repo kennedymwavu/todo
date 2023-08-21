847889c293230760f4f2d9a21fc6f5fd3a1aee5f
kennedymwavu <mwavukennedy@gmail.com>
2023-08-21 20:03:34 +0300

    ft: add plumber pipeline

---
+++ run_api.R
@@ -0,0 +1,11 @@
+conn <- DBI::dbConnect(drv = RSQLite::SQLite(), Sys.getenv("db_name"))
+
+# plumb----
+plumber::pr(file = "todo.R") |>
+  plumber::pr_hook(
+    stage = "exit",
+    handler = \(req) {
+      DBI::dbDisconnect(conn)
+    }
+  ) |>
+  plumber::pr_run(port = 8000)
